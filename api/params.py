fdd207adfcad603c68ae950c8eb3b2f07f157ba7
Alen Medjimorec <alen.medjimorec@gmail.com>
2021-12-01 17:17:58 +0100

    adding api folder - initial push

---
+++ api/params.py
@@ -0,0 +1,11 @@
+
+GCP_PATH = "frontend_video"
+
+IMAGE_HEIGHT = 64
+
+IMAGE_WIDTH = 64
+
+CLASSES_LIST = 
+['MilitaryParade', 'IceDancing', 'CricketShot', 'Fencing', 'JugglingBalls', 'Swing', 'RockClimbingIndoor',\
+'PlayingFlute', 'SalsaSpin', 'CricketBowling', 'Typing', 'ApplyEyeMakeup', 'PlayingTabla', 'BalanceBeam', 'FloorGymnastics',\
+'HeadMassage', 'FrisbeeCatch', 'Rowing', 'Hammering', 'CuttingInKitchen', 'BenchPress', 'PushUps', 'Nunchucks', 'Archery', 'LongJump']
