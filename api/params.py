63fef6ee3c5929fc881b1c15d0a6de7f9f6ae2ea
Phil Kolling <phil@kolling.de>
2021-12-01 19:59:04 +0100

    changed some files

--- api/params.py
+++ api/params.py
@@ -5,7 +5,6 @@ IMAGE_HEIGHT = 64
 
 IMAGE_WIDTH = 64
 
-CLASSES_LIST = 
-['MilitaryParade', 'IceDancing', 'CricketShot', 'Fencing', 'JugglingBalls', 'Swing', 'RockClimbingIndoor',\
-'PlayingFlute', 'SalsaSpin', 'CricketBowling', 'Typing', 'ApplyEyeMakeup', 'PlayingTabla', 'BalanceBeam', 'FloorGymnastics',\
+CLASSES_LIST = ['MilitaryParade', 'IceDancing', 'CricketShot', 'Fencing', 'JugglingBalls', 'Swing', 'RockClimbingIndoor',
+'PlayingFlute', 'SalsaSpin', 'CricketBowling', 'Typing', 'ApplyEyeMakeup', 'PlayingTabla', 'BalanceBeam', 'FloorGymnastics',
 'HeadMassage', 'FrisbeeCatch', 'Rowing', 'Hammering', 'CuttingInKitchen', 'BenchPress', 'PushUps', 'Nunchucks', 'Archery', 'LongJump']
